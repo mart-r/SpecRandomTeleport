e68e5f6ffd5c3c6eec19372807425bd4c63f5a9d
Mart Ratas <mart.ratas@gmail.com>
2020-04-18 07:51:52 +0300

    Made commands depend on the interface rather than the plugin

--- src/main/java/me/ford/srt/commands/SRTCommand.java
+++ src/main/java/me/ford/srt/commands/SRTCommand.java
@@ -1,6 +1,6 @@
 package me.ford.srt.commands;
 
-import me.ford.srt.SpecRandomTeleport;
+import me.ford.srt.ISpecRandomTeleport;
 import me.ford.srt.commands.subcommands.AddActivationSub;
 import me.ford.srt.commands.subcommands.AddLocationSub;
 import me.ford.srt.commands.subcommands.ListActivationsSub;
@@ -13,7 +13,7 @@ import me.ford.srt.commands.subcommands.UseSub;
 public class SRTCommand extends ParentCommand {
     private static final String USAGE = "/srt <subcommand> <args>";
 
-    public SRTCommand(SpecRandomTeleport srt) {
+    public SRTCommand(ISpecRandomTeleport srt) {
         super(srt.getMessages());
         addSubCommand("addloc", new AddLocationSub(srt.getLocationProvider(), srt.getMessages()));
         addSubCommand("removeloc", new RemoveLocationSub(srt.getLocationProvider(), srt.getMessages()));
