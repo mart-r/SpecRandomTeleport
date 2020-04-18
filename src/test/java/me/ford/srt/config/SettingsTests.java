39939db4f97d456febfaeab5d01afe6ebbc83831
Mart Ratas <mart.ratas@gmail.com>
2020-04-18 09:59:08 +0300

    Added test for settings

---
+++ src/test/java/me/ford/srt/config/SettingsTests.java
@@ -0,0 +1,30 @@
+package me.ford.srt.config;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import me.ford.srt.mock.MockSpecRandomTeleport;
+
+public class SettingsTests {
+    private MockSpecRandomTeleport srt;
+    private Settings settings;
+
+    @Before
+    public void setup() {
+        srt = new MockSpecRandomTeleport();
+        settings = srt.getSettings();
+    }
+
+    @After
+    public void tearDown() {
+        srt.disable();
+    }
+
+    @Test
+    public void methods() {
+        Assert.assertFalse(settings.isDebugEnabled());
+    }
+
+}
