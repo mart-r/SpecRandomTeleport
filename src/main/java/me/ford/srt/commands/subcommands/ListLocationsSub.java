4be78f613365c61bd55cef2bd54a48d48356cbdd
Mart Ratas <mart.ratas@gmail.com>
2020-04-19 15:05:24 +0300

    Added world as optional argument to the list command

--- src/main/java/me/ford/srt/commands/subcommands/ListLocationsSub.java
+++ src/main/java/me/ford/srt/commands/subcommands/ListLocationsSub.java
@@ -3,16 +3,18 @@ package me.ford.srt.commands.subcommands;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.util.StringUtil;
 
 import me.ford.srt.commands.SubCommand;
 import me.ford.srt.config.Messages;
 import me.ford.srt.locations.perworld.ComplexLocationProvider;
 
 public class ListLocationsSub extends SubCommand {
-    private static final String USAGE = "/srt list";
+    private static final String USAGE = "/srt list [world]";
     private static final String PERMS = "srt.commands.list";
     private final ComplexLocationProvider provider;
     private final Messages messages;
@@ -24,6 +26,13 @@ public class ListLocationsSub extends SubCommand {
 
     @Override
     public List<String> onTabComplete(CommandSender sender, String[] args) {
+        if (args.length == 1) {
+            List<String> list = new ArrayList<>();
+            for (World world : Bukkit.getWorlds()) {
+                list.add(world.getName());
+            }
+            return StringUtil.copyPartialMatches(args[0], list, new ArrayList<>());
+        }
         return new ArrayList<>();
     }
 
@@ -32,6 +41,12 @@ public class ListLocationsSub extends SubCommand {
         World world = null;
         if (sender instanceof Player) {
             world = ((Player) sender).getWorld();
+        }
+        if (args.length > 0) {
+            World reqWorld = Bukkit.getWorld(args[0]);
+            if (reqWorld != null) {
+                world = reqWorld;
+            }
         }
         sender.sendMessage(messages.getListMessage(provider.getLocations(world)));
         return true;
