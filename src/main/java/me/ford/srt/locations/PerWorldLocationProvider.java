37b4442de3b08bb76fad656f6e7b282c00820ed5
Mart Ratas <mart.ratas@gmail.com>
2020-04-19 09:14:36 +0300

    Added per world location provider (interface and implementation)

---
+++ src/main/java/me/ford/srt/locations/PerWorldLocationProvider.java
@@ -0,0 +1,90 @@
+package me.ford.srt.locations;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+
+import me.ford.srt.ISpecRandomTeleport;
+
+public class PerWorldLocationProvider implements PerWorldNamedLocationProvider {
+    private static final String FILE_NAME_FORMAT = "locations_%s.yml";
+    private final ISpecRandomTeleport srt;
+    private final Map<World, LocationProvider> worldProviders = new HashMap<>();
+
+    public PerWorldLocationProvider(ISpecRandomTeleport srt) {
+        this.srt = srt;
+    }
+
+    private LocationProvider getWorldProvider(World world) {
+        return worldProviders.get(world);
+    }
+
+    private LocationProvider getOrCreateWorldProvider(World world) {
+        LocationProvider provider = worldProviders.get(world);
+        if (provider == null) {
+            provider = new LocationProvider(srt, String.format(FILE_NAME_FORMAT, world.getName()));
+            worldProviders.put(world, provider);
+        }
+        return provider;
+    }
+
+    @Override
+    public boolean hasLocation(String name) {
+        for (LocationProvider provider : worldProviders.values()) {
+            if (provider.hasLocation(name))
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void setLocation(String name, Location loc) {
+        LocationProvider provider = getOrCreateWorldProvider(loc.getWorld());
+        provider.setLocation(name, loc);
+    }
+
+    @Override
+    public Location removeLocation(World world, String name) {
+        LocationProvider provider = getWorldProvider(world);
+        if (provider == null)
+            return null;
+        return provider.removeLocation(name);
+    }
+
+    @Override
+    public Location getLocation(World world, String name) {
+        LocationProvider provider = getWorldProvider(world);
+        if (provider == null)
+            return null;
+        return provider.getLocation(name);
+    }
+
+    @Override
+    public Map<String, NamedLocation> getLocations(World world) {
+        LocationProvider provider = getWorldProvider(world);
+        if (provider == null)
+            return new HashMap<>();
+        return provider.getLocations();
+    }
+
+    @Override
+    public Set<String> getNames(World world) {
+        LocationProvider provider = getWorldProvider(world);
+        if (provider == null)
+            return new HashSet<>();
+        return provider.getNames();
+    }
+
+    @Override
+    public NamedLocation getRandomLocation(World world) {
+        LocationProvider provider = getWorldProvider(world);
+        if (provider == null)
+            return null;
+        return provider.getRandomLocation();
+    }
+
+}
---
+++ src/main/java/me/ford/srt/locations/PerWorldNamedLocationProvider.java
@@ -0,0 +1,25 @@
+package me.ford.srt.locations;
+
+import java.util.Map;
+import java.util.Set;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+
+public interface PerWorldNamedLocationProvider {
+
+    public boolean hasLocation(String name);
+
+    public void setLocation(String name, Location loc);
+
+    public Location removeLocation(World world, String name);
+
+    public Location getLocation(World world, String name);
+
+    public Map<String, NamedLocation> getLocations(World world);
+
+    public Set<String> getNames(World world);
+
+    public NamedLocation getRandomLocation(World world);
+
+}
