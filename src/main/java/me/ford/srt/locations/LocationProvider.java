64ceb0dbedbe58316a2c27a59c72338553a135f4
Mart Ratas <mart.ratas@gmail.com>
2020-04-19 08:46:20 +0300

    Indentation and formatting changes

--- src/main/java/me/ford/srt/locations/LocationProvider.java
+++ src/main/java/me/ford/srt/locations/LocationProvider.java
@@ -3,10 +3,10 @@ package me.ford.srt.locations;
 import me.ford.srt.ISpecRandomTeleport;
 
 public class LocationProvider extends AbstractLocationProvider {
-  private static final String NAME = "locations.yml";
+    private static final String NAME = "locations.yml";
 
-  public LocationProvider(ISpecRandomTeleport srt) {
-    super(srt, NAME);
-  }
+    public LocationProvider(ISpecRandomTeleport srt) {
+        super(srt, NAME);
+    }
 
 }
